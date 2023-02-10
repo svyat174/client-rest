b3460f7ca94926b427fdc04f4921b820c2852717
svyat174 <110224503+svyat174@users.noreply.github.com>
2023-02-10 19:32:27 +0300

    setup my frontend app

---
+++ components/index.ts
@@ -0,0 +1,4 @@
+export * from './Htag/Htag';
+export * from './ButtonCostum/ButtonCostum';
+export * from './P/P';
+export * from './Tag/Tag';
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,21 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { AppProps } from "next/app";
+import '../styles/globals.css'
+import Head from "next/head";
+import React from "react";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+
+export default function Home({ Component, pageProps }: AppProps): JSX.Element {
+  return (
+    <>
+      <Head>
+        <title>Volosova - обучение, студия наращивания волос</title>
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://fonts.googleapis.com"/>
+        <link rel="preconnect" href="https://fonts.gstatic.com"/>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"/>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons"/>
+      </Head>
+      <Component { ...pageProps } />
+    </>
+  )
 }
